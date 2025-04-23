f009cda2b1145c2db4d923329902f63347fc8c95
amiina0901 <210302140@student.ius.edu.ba>
2025-04-23 13:54:07 +0200

    username issue Fix #4

--- BE/Program.cs
+++ BE/Program.cs
@@ -92,7 +92,7 @@ builder.Services.AddSingleton(mapper);
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
-        policy.WithOrigins("http://localhost:4200")
+        policy.WithOrigins("http://localhost:4200" , "https://heyappo.me")
               .AllowCredentials()
               .AllowAnyMethod()
               .AllowAnyHeader());
