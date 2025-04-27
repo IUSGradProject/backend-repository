b458e719603cac03cce1d4d8a30910ded4c3fc9a
amiina0901 <210302140@student.ius.edu.ba>
2025-04-27 20:12:30 +0200

    Add description

--- DTOs/Contracts/Product/ProductContract.cs
+++ DTOs/Contracts/Product/ProductContract.cs
@@ -9,6 +9,7 @@
         public Guid ProductId { get; set; }
         public string Name { get; set; }
         public string Image { get; set; }
+        public string? Description { get; set; }
         public float Price { get; set; }
         public int Available { get; set; }
         public int Brand { get; set;  }
